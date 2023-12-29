ab69b7b2710c3d4be8208f633606c7fb9d645d36
Eric Apostal <ehapostal04@gmail.com>
2023-12-28 21:08:17 -0500

    fix world scope collisions

--- src/client/game/vehicle/mesh.ts
+++ src/client/game/vehicle/mesh.ts
@@ -21,6 +21,10 @@ export class VehicleMesh extends BaseComponent implements OnStart {
         this.damageResistance = damageResistance;
     }
 
+    private localToGlobalVert(vert: number) {
+        return this.mesh!.GetPosition(vert).mul(this.scaling).add((this.instance as BasePart).Position);
+    }
+
     onStart() {
         // (this.instance as BasePart).Anchored = true;
         this.mesh = AssetService.CreateEditableMeshFromPartAsync(this.instance as MeshPart);
@@ -35,13 +39,15 @@ export class VehicleMesh extends BaseComponent implements OnStart {
             marker.Position = relativeCFrame.Position;
             marker.Size = new Vector3(0.1, 0.1, 0.1);
             marker.Anchored = false;
-            marker.Transparency = 0;
+            marker.Transparency = 1;
             marker.CanCollide = true
             marker.CollisionGroup = "car";
             marker.CanTouch = true;
             marker.Parent = game.Workspace;
             marker.Name = "marker";
             marker.Massless = true;
+            marker.Color = Color3.fromRGB(0, 255, 133);
+
             marker.AddTag("marker")
 
             let weld = new Instance("WeldConstraint");
@@ -49,9 +55,11 @@ export class VehicleMesh extends BaseComponent implements OnStart {
             weld.Part0 = marker;
             weld.Part1 = this.instance as BasePart;
 
-            marker.Touched.Connect((part) => {
-                task.wait();
 
+            marker.Touched.Connect((part) => {
+                if ((part.Parent as BasePart).Name === "wheels") return;
+                // if (((part.Parent as BasePart) as BasePart).Name === "baseplate") return;
+                task.wait();
 
                 if (((marker.GetAttribute("isTouched") === false) || (marker.GetAttribute("isTouched") === undefined))) {
                     marker.SetAttribute("isTouched", true);
@@ -59,18 +67,59 @@ export class VehicleMesh extends BaseComponent implements OnStart {
                     let curr = this.verticesMap.get(marker)!;
                     let currPos = this.mesh!.GetPosition(curr)!;
 
-                    let force = math.min(((part.GetVelocityAtPosition(currPos).Magnitude) + (this.instance as BasePart).GetVelocityAtPosition(currPos).Magnitude) / this.damageResistance, 0.5);
-                    print(force)
-                    let newPos = currPos.sub((part.Position.sub(marker.Position)).mul(force));
+                    let localImpactDirection = part.Position.sub(marker.Position).Unit;
+                    let globalImpactDirection = (this.instance as BasePart).CFrame.VectorToWorldSpace(localImpactDirection);
+
+                    let relativeVelocity = (this.instance as BasePart).AssemblyLinearVelocity;
+                    let impactSpeed = relativeVelocity.Magnitude;
+                    let forceMagnitude = math.min((impactSpeed) / this.damageResistance, 100000);
+
+                    let damageIntensity = math.min((forceMagnitude * 2) * part.GetMass() / 75, 8); // Increase intensity based on speed
+
+                    let newPos = currPos.add(globalImpactDirection.mul(-damageIntensity));
+
+                    if (currPos.sub(newPos).Magnitude >= 5) {
+                        marker.SetAttribute("isTouched", false);
+                        return;
+                    }
+                    let adjacents = this.mesh!.GetAdjacentVertices(curr) as Array<number>;
+                    let closestVert = undefined;
+                    for (let vert of adjacents) {
+                        let vertPos = this.mesh!.GetPosition(vert);
+                        if (closestVert === undefined) {
+                            closestVert = vert;
+                            continue;
+                        }
+                        if (vertPos.sub((this.instance as BasePart).Position).Magnitude < this.mesh!.GetPosition(closestVert).sub((this.instance as BasePart).Position).Magnitude) {
+                            closestVert = vert;
+                        }
+                    }
+
+                    for (let vert of adjacents) {
+                        let vertPos = this.mesh!.GetPosition(vert);
+                        // let newVertPos = vertPos.add(globalImpactDirection.mul(-damageIntensity / 2));
+                        // this.mesh!.SetPosition(vert, newVertPos);
+                        if (vertPos.sub(currPos).Magnitude > 20) {
+                            // remove current vertex
+                            print("too big!")
+                            this.mesh!.SetPosition(curr, this.mesh!.GetPosition(closestVert!));
+                        }
+                    }
+
                     this.mesh!.SetPosition(curr, newPos);
 
                     let markerCFrame = (this.instance as BasePart).CFrame.mul(new CFrame(this.mesh!.GetPosition(this.vertices[i]).mul(this.scaling)));
                     marker.Position = markerCFrame.Position;
 
-                    task.wait(2);
                     marker.SetAttribute("isTouched", false);
+                    marker.Color = Color3.fromRGB(250, 46, 46);
+                    task.wait(3);
+                    marker.Color = Color3.fromRGB(0, 255, 133);
                 }
             });
+
+
+
 
             // silly, but allows for O(1) lookup
             this.markerMap.set(this.vertices[i], marker);
